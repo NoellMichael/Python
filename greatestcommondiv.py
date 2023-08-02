2860adf555accbcc6a2c93b8f2df235c8dc4f226
Noell Biju Michael <noellbijumichael@gmail.com>
2023-08-02 19:27:36 +0530

    new

---
+++ greatestcommondiv.py
@@ -0,0 +1,16 @@
+#gretest common divisor
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+
+
+def gcdfunction(num1, num2):
+    if num1 > num2:
+        small = num2
+    else:
+        small = num1
+    for i in range(1, small+1):
+        if (num1 % i == 0) and (num2 % i == 0):
+            gcd = i
+    print("GCD of two Number: ",gcd)
+
+gcdfunction(num1, num2)
